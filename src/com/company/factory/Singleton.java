66a3e7de6c564d62ac5b1ad650b0e1e7d3ed5504
Joker <2635225112@qq.com>
2020-06-09 12:27:25 +0800

    单例模式-静态内部类

--- src/com/company/factory/Singleton.java
+++ src/com/company/factory/Singleton.java
@@ -1,24 +1,16 @@
 package com.company.factory;
 /**
- 优点：在获取实例的方法中，进行实例的初始化，节省系统资源
+ 优点：既避免了同步带来的性能损耗，又能够延迟加载
 
- 缺点：①如果获取实例时，初始化工作较多，加载速度会变慢，影响系统系能
-
- ②每次获取实例都要进行非空检查，系统开销大
-
- ③非线程安全，当多个线程同时访问getInstance()时，可能会产生多个实例
  */
 public class Singleton {
-    private static Singleton instance = null;
     private Singleton() {
     }
+    private static class SingletonHolder{
+        private static Singleton singleton = new Singleton();
+    }
 
-    public static synchronized Singleton getInstance() {
-        synchronized (Singleton.class) {
-            if (instance == null) {
-                instance = new Singleton();
-            }
-        }
-        return instance;
+    public static  Singleton getInstance() {
+        return SingletonHolder.singleton;
     }
 }
